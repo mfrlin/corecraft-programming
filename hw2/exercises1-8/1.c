e01a03560dfbd8dcda7c569a354e7bf871325733
ragezor <ragezor@debian>
2013-12-27 06:04:09 +0100

    Exercise from section 1.8

---
+++ hw2/exercises1-8/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void increment(int x);
+
+int main(void) {
+	// name x also displays scoping
+	int x = 100;
+	int n = 0;
+	increment(n);
+	increment(x);
+	printf("%d\n", n);
+	printf("%d\n", x);
+
+	return 0;
+}
+
+void increment(int x) {
+	x += 1;
+} 
